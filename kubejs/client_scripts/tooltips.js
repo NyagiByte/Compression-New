9e92fe227358544205e1e80e87992c1d9eece7b3
Micomi <zelverilun@gmail.com>
2026-04-18 19:15:10 -0700

    More item border groundwork (and adding more to tier 0 category)

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -4,5 +4,13 @@ ItemEvents.tooltip(event => {
 
     event.add(['#forge:tier_groups/tier_1'],
     '§bTier 1'
+    ),
+
+    event.add(['#forge:tier_groups/tier_2'],
+    '§6Tier 2'
+    ),
+
+    event.add(['#forge:tier_groups/tier_3'],
+    '§dTier 3'
     )
 })
