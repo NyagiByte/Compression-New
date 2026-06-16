8e8b34ab569caf491ea88909abccacdd810e37b9
Madelyn Byte <124478839+NyagiByte@users.noreply.github.com>
2026-06-16 13:59:43 -0400

    Update block_modify.js

--- kubejs/startup_scripts/block_modify.js
+++ kubejs/startup_scripts/block_modify.js
@@ -8,4 +8,10 @@ BlockEvents.modification(e => {
     e.modify('minecraft:respawn_anchor', block => {
       block.requiresTool = false
     })
+    e.modify({mod: 'abyssal_decor'}, block => {
+      block.requiresTool = false
+    })
+    e.modify({mod: 'architects_palette'}, block => {
+      block.requiresTool = false
+    })
   })
