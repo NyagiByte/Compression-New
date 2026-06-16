e503445a2008884540f844a17d370df810ee432c
Madelyn Byte <124478839+NyagiByte@users.noreply.github.com>
2026-06-16 11:28:38 -0400

    seed names

--- kubejs/client_scripts/item_descriptions.js
+++ kubejs/client_scripts/item_descriptions.js
@@ -1,3 +1,17 @@
 ItemEvents.tooltip(event => {
- event.add(['abyssal_decor:bottomless_bag_of_dirt'], '§7The Holy Dirtmongers of the Equinox slopes have invented this product to fill ever growing void in the market and hearts of dirt lovers worldwide! This proprietary technology handles the endless manufacture of consumer grade dirt through the emerging grime rebound technology that lines the very walls of this bag. No more will you run out of mana for your rod of the earth! Trust in us here at Grongocorp LTD. to take care of our customers and their growing soil needs!')
+ event.add(['abyssal_decor:bottomless_bag_of_dirt'], 
+    '§7The Holy Dirtmongers of the Equinox slopes have invented this product to fill ever growing void in the market and hearts of dirt lovers worldwide! This proprietary technology handles the endless manufacture of consumer grade dirt through the emerging grime rebound technology that lines the very walls of this bag. No more will you run out of mana for your rod of the earth! Trust in us here at Grongocorp LTD. to take care of our customers and their growing soil needs!'
+)
+ event.add(['minecraft:wheat_seeds'], 
+    '§7Species: Thinopyrum Intermedium'
+ )
+ event.add(['minecraft:pumpkin_seeds'], 
+    '§7Species: Cucurbita Pepo'
+ )
+ event.add(['minecraft:melon_seeds'], 
+    '§7Species: Citrullus Lanatus Cordophanus'
+ )
+ event.add(['minecraft:beetroot_seeds'], 
+    '§7Species: Citrullus Lanatus Cordophanus'
+ )
 })
