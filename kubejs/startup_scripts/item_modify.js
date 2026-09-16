d55324b2dcc188e42c1be335973660a209890e0d
Zelveri <zelverilun@gmail.com>
2026-09-16 19:09:25 -0400

    some items are now fire resistant

---
+++ kubejs/startup_scripts/item_modify.js
@@ -0,0 +1,32 @@
+ItemEvents.modification(e => {
+  e.modify('minecraft:netherrack', item => {
+    item.fireResistant = true
+  })
+  e.modify('minecraft:nether_bricks', item => {
+    item.fireResistant = true
+  })
+  e.modify('minecraft:basalt', item => {
+    item.fireResistant = true
+  })
+  e.modify('minecraft:blackstone', item => {
+    item.fireResistant = true
+  })
+  e.modify('minecraft:blackstone', item => {
+    item.fireResistant = true
+  })
+  e.modify('nyagibits_bytes:porous_insulating_firebrick', item => {
+    item.fireResistant = true
+  })
+  e.modify('botania:endoflame', item => {
+    item.fireResistant = true
+  })
+  e.modify('botania:floating_endoflame', item => {
+    item.fireResistant = true
+  })
+  e.modify('botania:thermalily', item => {
+    item.fireResistant = true
+  })
+  e.modify('botania:floating_thermalily', item => {
+    item.fireResistant = true
+  })
+})
